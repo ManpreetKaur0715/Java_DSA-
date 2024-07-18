eb4118cc30e6446504cf1212360a9c3ec22dfde8
Manpreetkaur0715 <you@example.com>
2024-07-19 00:06:49 +0530

    third commit

--- Daypractice.java/p3.java
+++ Daypractice.java/p3.java
@@ -1,16 +1,18 @@
 public class p3 {
     public static void main(String[] args) {
-        //first triangle
-       int n=5;
-       for (int i=n;i>=1;i--){
-       for(int k=1;k<=(n-i);k++)
-       System.out.print("  "); 
-    for(int j=1;j<=(i-1) && j<=n;j++)
-    System.out.print("* ");
-    System.out.println();
-    //second triangle
-    //for(int j=1;j<=(i-1)&&j<=n;j++)
-    //System.out.println("* ");
-    }
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int k=1;k<=n-i;k++){
+                System.out.print("  ");
+                }
+            for(int j=1;j<=i   ;j++){
+                System.out.print("* ");
+            }
+        
+        for(int j=1;j<=i-1;j++){
+          System.out.print("* ");
+        }
+        System.out.println();
+       }    
 }
 }
